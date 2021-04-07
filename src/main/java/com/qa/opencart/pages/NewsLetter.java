2f23cf42f503f25d3623bfffcb6d72056e7d42e6
axn09261983 <81985099+axn09261983@users.noreply.github.com>
2021-04-06 22:18:55 -0400

    Create NewsLetter.java
    added the newsletter file

---
+++ src/main/java/com/qa/opencart/pages/NewsLetter.java
@@ -0,0 +1,25 @@
+package com.qa.opencart.pages;
+
+
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+
+public class NewsLetterPage {
+	private WebDriver driver;	
+
+	
+	private By newsltter = By.xpath("//div[@id='newsletter']");
+
+	
+	
+	
+	public NewsLetterPagee() {
+	System.out.println("NewsLtter......");
+				
+	}
